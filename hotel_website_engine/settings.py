7178a2b055878461021b08c87f84b39cf1962dc2
Timieweb <timileyinoluwasheyi438@gmail.com>
2026-07-18 15:29:00 +0100

    set

--- hotel_website_engine/settings.py
+++ hotel_website_engine/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'core',
 ]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',   # Important for static files
@@ -53,7 +54,6 @@ TEMPLATES = [
     },
 ]
 
-
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
@@ -74,9 +74,11 @@ DATABASES = {
 # Static files
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'core/static')
+    os.path.join(BASE_DIR, 'core/static'),
 ]
+
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
 ]
@@ -95,12 +97,11 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_TLS = False
-EMAIL_USE_SSL =True
+EMAIL_USE_SSL = True
 
 EMAIL_HOST_USER = 'timileyinoluwasheyi438@gmail.com'
 EMAIL_HOST_PASSWORD = 'pabsldpddwzfdbmu'
