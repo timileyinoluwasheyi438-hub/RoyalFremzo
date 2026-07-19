f866b4d89867eb7fd653b5bcd29aaf76557a00b7
Timieweb <timileyinoluwasheyi438@gmail.com>
2026-07-19 16:06:40 +0100

    Switch email to port 587 TLS to fix SMTP timeout on Render

--- hotel_website_engine/settings.py
+++ hotel_website_engine/settings.py
@@ -95,9 +95,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 EMAIL_HOST_USER = 'timileyinoluwasheyi438@gmail.com'
 EMAIL_HOST_PASSWORD = 'pabsldpddwzfdbmu'
